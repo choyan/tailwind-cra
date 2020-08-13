0ce828d876b83342a414ff332ecb51a8fb0ac649
Zahidul Hossain <choyan.009@gmail.com>
2020-08-13 12:59:18 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link border-b-2 border-gray-200"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+// tailwind.config.js
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
+  theme: {},
+  variants: {},
+  plugins: [],
+};
